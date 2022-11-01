84ed66c962d8ad55384445ef2487ecd524c5ded8
planit <planit@tanjeffreyz.github.io>
2022-11-01 23:09:21 +0000

    Updated assignments

---
+++ data/assignments.js
@@ -0,0 +1,316 @@
+assignments = {
+  "Chem 1A  Fall 2022": [
+    {
+      "title": "Discussion Week 1: Lewis Dot Structures I",
+      "course": "Chem 1A  Fall 2022",
+      "dueDate": "2022-09-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/422087/assignments/2191647/submissions/131632564",
+      "submitted": true
+    },
+    {
+      "title": "Discussion Week 2: Lewis Dot Structures II",
+      "course": "Chem 1A  Fall 2022",
+      "dueDate": "2022-09-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/422087/assignments/2205168/submissions/131936265",
+      "submitted": true
+    },
+    {
+      "title": "Discussion Week 3: Gas Behavior",
+      "course": "Chem 1A  Fall 2022",
+      "dueDate": "2022-09-15T23:59:00",
+      "link": "https://www.gradescope.com/courses/422087/assignments/2205183/submissions/133016497",
+      "submitted": true
+    },
+    {
+      "title": "Discussion Week 4: Stoichiometry",
+      "course": "Chem 1A  Fall 2022",
+      "dueDate": "2022-09-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/422087/assignments/2251295/submissions/135785427",
+      "submitted": true
+    },
+    {
+      "title": "Discussion Week 5: Equilibrium",
+      "course": "Chem 1A  Fall 2022",
+      "dueDate": "2022-09-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/422087/assignments/2251305/submissions/136299515",
+      "submitted": true
+    },
+    {
+      "title": "Discussion Week 6: Strong and Weak Acids and Bases",
+      "course": "Chem 1A  Fall 2022",
+      "dueDate": "2022-10-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/422087/assignments/2251307/submissions/137986877",
+      "submitted": true
+    },
+    {
+      "title": "Discussion Week 7: Buffers and Other Equilibria",
+      "course": "Chem 1A  Fall 2022",
+      "dueDate": "2022-10-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/422087",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Week 8: Thermo I: Heat and Work",
+      "course": "Chem 1A  Fall 2022",
+      "dueDate": "2022-10-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/422087",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Week 9: Thermo II: Bond Energy",
+      "course": "Chem 1A  Fall 2022",
+      "dueDate": "2022-10-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/422087",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Week 10: Thermo III: Gibb's Free Energy",
+      "course": "Chem 1A  Fall 2022",
+      "dueDate": "2022-11-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/422087",
+      "submitted": false
+    }
+  ],
+  "Data 8": [
+    {
+      "title": "Lab 01 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-08-26T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2177074/submissions/130885289",
+      "submitted": true
+    },
+    {
+      "title": "HW 01 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-08-31T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2183546/submissions/131423780",
+      "submitted": true
+    },
+    {
+      "title": "Lab 02 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-09-02T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2204429/submissions/131565585",
+      "submitted": true
+    },
+    {
+      "title": "HW 02 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-09-07T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2207322/submissions/132478540",
+      "submitted": true
+    },
+    {
+      "title": "Lab 03 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-09-09T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2226780/submissions/132525955",
+      "submitted": true
+    },
+    {
+      "title": "HW 03 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-09-14T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2226814/submissions/133603661",
+      "submitted": true
+    },
+    {
+      "title": "Lab 04 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-09-16T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2246022/submissions/133934562",
+      "submitted": true
+    },
+    {
+      "title": "HW 04 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-09-21T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2246028/submissions/135497628",
+      "submitted": true
+    },
+    {
+      "title": "Project 1 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-09-30T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2262862/submissions/137464258",
+      "submitted": true
+    },
+    {
+      "title": "Project 1 Checkpoint",
+      "course": "Data 8",
+      "dueDate": "2022-09-23T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2262865/submissions/135553554",
+      "submitted": true
+    },
+    {
+      "title": "Lab 05 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-09-30T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2268083/submissions/137192062",
+      "submitted": true
+    },
+    {
+      "title": "HW 05 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-09-28T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2268265/submissions/136699243",
+      "submitted": true
+    },
+    {
+      "title": "HW 06 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-10-05T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2300884/submissions/138141202",
+      "submitted": true
+    },
+    {
+      "title": "Lab 06 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-10-08T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2313053/submissions/138909633",
+      "submitted": true
+    },
+    {
+      "title": "HW 07 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-10-12T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2313065/submissions/140433520",
+      "submitted": true
+    },
+    {
+      "title": "Lab 07 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-10-21T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2352425/submissions/142410007",
+      "submitted": true
+    },
+    {
+      "title": "HW 08 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-10-26T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2355910/submissions/143764396",
+      "submitted": true
+    },
+    {
+      "title": "Lab 08 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-10-28T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727/assignments/2377008/submissions/144307931",
+      "submitted": true
+    },
+    {
+      "title": "HW 09 Autograder",
+      "course": "Data 8",
+      "dueDate": "2022-11-02T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727",
+      "submitted": false
+    },
+    {
+      "title": "Project 2 Checkpoint",
+      "course": "Data 8",
+      "dueDate": "2022-11-04T23:00:00",
+      "link": "https://www.gradescope.com/courses/415727",
+      "submitted": false
+    }
+  ],
+  "MATH 1B": [
+    {
+      "title": "Homework 1",
+      "course": "MATH 1B",
+      "dueDate": "2022-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202136/submissions/131183687",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202150/submissions/132285265",
+      "submitted": true
+    },
+    {
+      "title": "Homework 3",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202151/submissions/133614886",
+      "submitted": true
+    },
+    {
+      "title": "Homework 4",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202152/submissions/135794187",
+      "submitted": true
+    },
+    {
+      "title": "Homework 5",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202153/submissions/138530945",
+      "submitted": true
+    },
+    {
+      "title": "Homework 6",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202154/submissions/138550118",
+      "submitted": true
+    },
+    {
+      "title": "Homework 7",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-11T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202155/submissions/140244991",
+      "submitted": true
+    },
+    {
+      "title": "Homework 8",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202156/submissions/141996426",
+      "submitted": true
+    },
+    {
+      "title": "Homework 9",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202157/submissions/144912125",
+      "submitted": true
+    },
+    {
+      "title": "Homework 10",
+      "course": "MATH 1B",
+      "dueDate": "2022-11-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777",
+      "submitted": false
+    },
+    {
+      "title": "Project 1",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202199/submissions/132921425",
+      "submitted": true
+    },
+    {
+      "title": "Project 2",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202218/submissions/137714564",
+      "submitted": true
+    },
+    {
+      "title": "Project 3",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777",
+      "submitted": false
+    },
+    {
+      "title": "Project 4",
+      "course": "MATH 1B",
+      "dueDate": "2022-11-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777",
+      "submitted": false
+    }
+  ]
+};
