7bd4ed955510544ed4989d046fb93fc2902009ef
planit <planit@tanjeffreyz.github.io>
2022-11-14 07:38:35 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -81,8 +81,8 @@ assignments = {
       "title": "Discussion Week 12: Quantum I",
       "course": "Chem 1A  Fall 2022",
       "dueDate": "2022-11-17T23:59:00",
-      "link": "https://www.gradescope.com/courses/422087",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/422087/assignments/2423963/submissions/148640809",
+      "submitted": true
     }
   ],
   "Data 8": [
