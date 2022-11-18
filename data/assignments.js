c97df588322b93fca661a94663ef0f1adbe593b3
planit <planit@tanjeffreyz.github.io>
2022-11-18 21:37:47 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -87,7 +87,7 @@ assignments = {
     {
       "title": "Discussion Week 13: Quantum II",
       "course": "Chem 1A  Fall 2022",
-      "dueDate": "2022-11-24T23:59:00",
+      "dueDate": "2022-11-28T23:59:00",
       "link": "https://www.gradescope.com/courses/422087",
       "submitted": false
     }
