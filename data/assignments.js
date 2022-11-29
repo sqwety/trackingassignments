4b7395d92cc30303b539d0dd9b8380393e7270e7
planit <planit@tanjeffreyz.github.io>
2022-11-29 02:10:06 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -274,13 +274,6 @@ assignments = {
       "dueDate": "2022-11-18T23:00:00",
       "link": "https://www.gradescope.com/courses/415727/assignments/2426621/submissions/150028664",
       "submitted": true
-    },
-    {
-      "title": "Lab 10 Autograder",
-      "course": "Data 8",
-      "dueDate": "2022-12-02T23:00:00",
-      "link": "https://www.gradescope.com/courses/415727",
-      "submitted": false
     }
   ],
   "MATH 1B": [
