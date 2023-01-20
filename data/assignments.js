109bd38f326f68a60ed73fdb26ac770a7b32f856
planit <planit@tanjeffreyz.github.io>
2023-01-20 02:04:13 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,12 @@
 assignments = {
   "7A 001/003": [],
-  "MATH 53 SP 23": []
+  "MATH 53 SP 23": [
+    {
+      "title": "Homework 1",
+      "course": "MATH 53 SP 23",
+      "dueDate": "2023-01-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/498818",
+      "submitted": false
+    }
+  ]
 };
