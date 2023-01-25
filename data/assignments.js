7f9a35ca9ea5900661a1a14b974062ac00474ced
planit <planit@tanjeffreyz.github.io>
2023-01-25 04:39:17 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,9 +1,9 @@
 assignments = {
   "7A 001/003": [
     {
-      "title": "Week 2 Homework (Ch 2)",
+      "title": "Week 2 Homework (Ch 2) ",
       "course": "7A 001/003",
-      "dueDate": "2023-01-27T23:59:00",
+      "dueDate": "2023-01-24T21:00:00",
       "link": "https://www.gradescope.com/courses/495805",
       "submitted": false
     },
