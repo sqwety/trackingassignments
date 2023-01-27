fd2911a823b81f3ff35669b970c4abb81d3fdafa
planit <planit@tanjeffreyz.github.io>
2023-01-27 22:35:42 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -11,8 +11,8 @@ assignments = {
       "title": "Week 2 Homework (Ch 2) VERSION 2",
       "course": "7A 001/003",
       "dueDate": "2023-01-27T23:59:00",
-      "link": "https://www.gradescope.com/courses/495805",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/495805/assignments/2603660/submissions/157731585",
+      "submitted": true
     }
   ],
   "MATH 53 SP 23": [
