ea67cc823aac516f6f51dbcc487f4dc7d1cf0536
planit <planit@tanjeffreyz.github.io>
2023-02-09 15:38:16 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,49 +1,5 @@
 assignments = {
-  "7A 001/003": [
-    {
-      "title": "Week 2 Homework (Ch 2) ",
-      "course": "7A 001/003",
-      "dueDate": "2023-01-24T21:00:00",
-      "link": "https://www.gradescope.com/courses/495805",
-      "submitted": false
-    },
-    {
-      "title": "Week 2 Homework (Ch 2) VERSION 2",
-      "course": "7A 001/003",
-      "dueDate": "2023-01-27T23:59:00",
-      "link": "https://www.gradescope.com/courses/495805/assignments/2603660/submissions/157749638",
-      "submitted": true
-    },
-    {
-      "title": "Week 3 Homework (Ch 3)",
-      "course": "7A 001/003",
-      "dueDate": "2023-02-03T23:59:00",
-      "link": "https://www.gradescope.com/courses/495805/assignments/2618084/submissions/159495583",
-      "submitted": true
-    },
-    {
-      "title": "Week 4 Homework (Ch 4)",
-      "course": "7A 001/003",
-      "dueDate": "2023-02-10T23:59:00",
-      "link": "https://www.gradescope.com/courses/495805",
-      "submitted": false
-    }
-  ],
-  "MATH 53 SP 23": [
-    {
-      "title": "Homework 1",
-      "course": "MATH 53 SP 23",
-      "dueDate": "2023-01-30T23:59:00",
-      "link": "https://www.gradescope.com/courses/498818/assignments/2592311/submissions/158125558",
-      "submitted": true
-    },
-    {
-      "title": "Homework 2",
-      "course": "MATH 53 SP 23",
-      "dueDate": "2023-02-06T23:59:00",
-      "link": "https://www.gradescope.com/courses/498818/assignments/2637943/submissions/160060045",
-      "submitted": true
-    }
-  ],
+  "7A 001/003": [],
+  "MATH 53 SP 23": [],
   "Math 53": []
 };
