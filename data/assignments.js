8686c81a9d9635f909c1733fc5e56d690d7520db
planit <planit@tanjeffreyz.github.io>
2023-06-18 23:35:54 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -4,8 +4,8 @@ assignments = {
       "title": "Being a Good Classmate",
       "course": "CS 61BL",
       "dueDate": "2023-06-25T23:59:00",
-      "link": "https://www.gradescope.com/courses/545060/assignments/2943575/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/545060/assignments/2943575/submissions/183668836",
+      "submitted": true
     }
   ]
 };
