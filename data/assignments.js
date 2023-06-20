8d5a763c93cfe9a885d2e4923a13d3e4d98bb414
planit <planit@tanjeffreyz.github.io>
2023-06-20 03:36:02 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,6 +1,13 @@
 assignments = {
   "CS 61BL": [
     {
+      "title": "Lab 1: Java and Git",
+      "course": "CS 61BL",
+      "dueDate": "2023-06-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
       "title": "Being a Good Classmate",
       "course": "CS 61BL",
       "dueDate": "2023-06-25T23:59:00",
