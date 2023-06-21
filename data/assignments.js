c6511648ef208cdfba547f95429ea608db5d634b
planit <planit@tanjeffreyz.github.io>
2023-06-21 01:54:01 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -13,6 +13,13 @@ assignments = {
       "dueDate": "2023-06-25T23:59:00",
       "link": "https://www.gradescope.com/courses/545060/assignments/2943575/submissions/183668836",
       "submitted": true
+    },
+    {
+      "title": "Project 0: 2048",
+      "course": "CS 61BL",
+      "dueDate": "2023-06-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
     }
   ]
 };
