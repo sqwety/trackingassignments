59a0b6b088a6c19da041615bf1f2cc5eea41c3a7
planit <planit@tanjeffreyz.github.io>
2023-07-09 04:38:16 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -160,6 +160,13 @@ assignments = {
       "dueDate": "2023-07-10T23:59:00",
       "link": "https://www.gradescope.com/courses/545060/assignments/2969990/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "Remote Practice Exam",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-14T09:30:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
     }
   ]
 };
