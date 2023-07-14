37031e5672730d67042225ee81cca33037eebed9
planit <planit@tanjeffreyz.github.io>
2023-07-14 07:36:12 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -186,7 +186,7 @@ assignments = {
       "title": "Redemption Quiz 3",
       "course": "CS 61BL",
       "dueDate": "2023-07-13T23:59:00",
-      "link": "https://www.gradescope.com/courses/545060/assignments/2975880/submissions/new",
+      "link": "https://www.gradescope.com/courses/545060",
       "submitted": false
     },
     {
