65ac7c96f8b627807f8da1ba85c4470f3a445839
planit <planit@tanjeffreyz.github.io>
2023-07-15 07:35:48 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -193,7 +193,7 @@ assignments = {
       "title": "[OPTIONAL] Quiz 4",
       "course": "CS 61BL",
       "dueDate": "2023-07-14T23:59:00",
-      "link": "https://www.gradescope.com/courses/545060/assignments/2976462/submissions/new",
+      "link": "https://www.gradescope.com/courses/545060",
       "submitted": false
     },
     {
