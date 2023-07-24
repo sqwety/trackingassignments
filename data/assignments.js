1ed8239b10f32958430cc76452494f6ec66854b3
planit <planit@tanjeffreyz.github.io>
2023-07-24 08:36:52 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -244,6 +244,27 @@ assignments = {
       "dueDate": "2023-07-19T00:00:00",
       "link": "https://www.gradescope.com/courses/545060",
       "submitted": false
+    },
+    {
+      "title": "Project 3 Phase 1: World Generation",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Project 3 Phase 2: Interactivity",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 16: BYOW Intro",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-24T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
     }
   ]
 };
