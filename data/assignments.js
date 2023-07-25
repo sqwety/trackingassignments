5de327cfb970cbb2172cad085a0274f2e19f3d48
planit <planit@tanjeffreyz.github.io>
2023-07-25 16:39:46 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -265,6 +265,13 @@ assignments = {
       "dueDate": "2023-07-24T04:00:00",
       "link": "https://www.gradescope.com/courses/545060",
       "submitted": false
+    },
+    {
+      "title": "Lab 17: Graphs",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-25T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
     }
   ]
 };
