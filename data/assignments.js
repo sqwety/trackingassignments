fbf6054248f88994e925d5b6f2d4d6b4dfe189ac
planit <planit@tanjeffreyz.github.io>
2023-07-27 07:35:39 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -274,6 +274,13 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "Redemption Quiz 5",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060/assignments/2990577/submissions/new",
+      "submitted": false
+    },
+    {
       "title": "Lab 18: Shortest Path Trees",
       "course": "CS 61BL",
       "dueDate": "2023-07-26T00:00:00",
