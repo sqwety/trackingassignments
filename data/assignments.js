8de85b425479bf0a232c916e4ee736b6ce35c282
planit <planit@tanjeffreyz.github.io>
2023-08-12 19:35:38 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,1 +1,396 @@
-assignments = {};
+assignments = {
+  "CS 61BL": [
+    {
+      "title": "Lab 1: Java and Git",
+      "course": "CS 61BL",
+      "dueDate": "2023-06-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060/assignments/2917541/submissions/183900750",
+      "submitted": true
+    },
+    {
+      "title": "Lab 2: Conditionals, Loops, Arrays, and an Introduction to Testing",
+      "course": "CS 61BL",
+      "dueDate": "2023-06-22T18:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 3: Test Driven Development and Debugging",
+      "course": "CS 61BL",
+      "dueDate": "2023-06-23T18:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Being a Good Classmate",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060/assignments/2943575/submissions/183668836",
+      "submitted": true
+    },
+    {
+      "title": "Project 0: 2048",
+      "course": "CS 61BL",
+      "dueDate": "2023-06-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Quiz 1",
+      "course": "CS 61BL",
+      "dueDate": "2023-06-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 4: Primitives and Objects",
+      "course": "CS 61BL",
+      "dueDate": "2023-06-27T18:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Project 1A: LinkedListDeque",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-05T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Project 1B: ArrayDeque",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-05T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Project 1C: Deque Enhancements",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-05T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 6: Inheritance (Non-coding)",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 6: Inheritance (Coding)",
+      "course": "CS 61BL",
+      "dueDate": "2023-06-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 7: Comparison, Exceptions, and Iteration",
+      "course": "CS 61BL",
+      "dueDate": "2023-06-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 5: Linked Lists",
+      "course": "CS 61BL",
+      "dueDate": "2023-04-10T18:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Redemption Quiz 1",
+      "course": "CS 61BL",
+      "dueDate": "2023-06-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Quiz 2",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 8: Persistence and Gitlet",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-04T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Project 2A: Gitlet Checkpoint",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Project 2: Gitlet",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 9: Runtime",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-06T14:52:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 10: Tree Traversals",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-06T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Redemption Quiz 2",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Quiz 3",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Remote Practice Exam",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-11T12:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 11: Binary Search Trees",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-10T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 12: Balanced Search Structures",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-11T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Redemption Quiz 3",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "[OPTIONAL] Quiz 4",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Quiz 5",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "[FOR REVIEW PURPOSES ONLY] Redemption Quiz 3",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060/assignments/2977888/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "[DSP] Midterm",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-14T22:30:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "[Alternate] Midterm",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-15T10:30:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 13: Hashing",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-17T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 14: Heaps and Priority Queues",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-18T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 15: Disjoint Sets",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-19T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Project 3 Phase 1: World Generation",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-31T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Project 3 Phase 2: Interactivity",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 16: BYOW Intro",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-24T04:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 17: Graphs",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-29T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Redemption Quiz 5",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 18: Shortest Path Trees",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-26T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Quiz 6",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-01T00:30:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 19: Minimum Spanning Trees",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-27T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "[FOR REVIEW PURPOSES ONLY] Quiz 5",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060/assignments/2992993/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Lab 20: Comparison-Based Sorts 1",
+      "course": "CS 61BL",
+      "dueDate": "2023-07-31T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 21: Comparison-Based Sorts 2",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-01T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 22: Counting Sorts",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-02T00:00:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Redemption Quiz 6",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-04T00:55:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Quiz 7",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 23: Final Review",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Lab 24: Summer 2022 Final",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "Redemption Quiz 7",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "[REVIEW PURPOSES ONLY] Lab 23: Final Review",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/545060/assignments/3038417/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "[DSP] Final",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-11T22:30:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "[Alternate 1] Final",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-11T18:30:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    },
+    {
+      "title": "[Alternate 2] Final",
+      "course": "CS 61BL",
+      "dueDate": "2023-08-12T11:30:00",
+      "link": "https://www.gradescope.com/courses/545060",
+      "submitted": false
+    }
+  ]
+};
