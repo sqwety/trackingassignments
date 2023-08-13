6619a6489b5f0422644588b704e4d2228cac30d2
planit <planit@tanjeffreyz.github.io>
2023-08-13 07:35:58 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -368,7 +368,7 @@ assignments = {
       "title": "[REVIEW PURPOSES ONLY] Lab 23: Final Review",
       "course": "CS 61BL",
       "dueDate": "2023-08-12T23:59:00",
-      "link": "https://www.gradescope.com/courses/545060/assignments/3038417/submissions/new",
+      "link": "https://www.gradescope.com/courses/545060",
       "submitted": false
     },
     {
