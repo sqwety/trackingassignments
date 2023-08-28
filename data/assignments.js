b0b5fe5cc8db220ddbbe945d7e30c648b0e10b29
planit <planit@tanjeffreyz.github.io>
2023-08-28 21:35:47 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,12 @@
 assignments = {
-  "CE 11": [],
+  "CE 11": [
+    {
+      "title": "Homework 1",
+      "course": "CE 11",
+      "dueDate": "2023-09-01T10:10:00",
+      "link": "https://www.gradescope.com/courses/584827",
+      "submitted": false
+    }
+  ],
   "CE 93": []
 };
