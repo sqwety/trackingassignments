46aa1f442f7aaed3a93a62369ef18d230630b66a
planit <planit@tanjeffreyz.github.io>
2023-10-13 19:35:47 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -50,6 +50,13 @@ assignments = {
       "dueDate": "2023-10-12T09:30:00",
       "link": "https://www.gradescope.com/courses/629573",
       "submitted": false
+    },
+    {
+      "title": "Field Trip Report",
+      "course": "CE 70",
+      "dueDate": "2023-10-19T08:00:00",
+      "link": "https://www.gradescope.com/courses/629573",
+      "submitted": false
     }
   ],
   "CE 93": [
