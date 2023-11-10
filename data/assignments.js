a209349246e3b0cb3d552bc53eff63683a398c4e
planit <planit@tanjeffreyz.github.io>
2023-11-10 19:35:41 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -99,6 +99,13 @@ assignments = {
       "dueDate": "2023-11-16T23:59:00",
       "link": "https://www.gradescope.com/courses/629573",
       "submitted": false
+    },
+    {
+      "title": "GIS #1",
+      "course": "CE 70",
+      "dueDate": "2023-11-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/629573",
+      "submitted": false
     }
   ],
   "CE 93": [
