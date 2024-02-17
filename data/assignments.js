1b430b78dd06cbaeae6cea069fa6fdc439d9227c
planit <planit@tanjeffreyz.github.io>
2024-02-17 08:36:01 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -27,6 +27,13 @@ assignments = {
       "dueDate": "2024-02-16T23:59:00",
       "link": "https://www.gradescope.com/courses/711745",
       "submitted": false
+    },
+    {
+      "title": "HW5",
+      "course": "CivEng C30/MecEng C85",
+      "dueDate": "2024-02-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/711745",
+      "submitted": false
     }
   ]
 };
