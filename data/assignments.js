1fdcd03f4023754f9efce615f4fe044834b83868
planit <planit@tanjeffreyz.github.io>
2024-10-01 05:40:43 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -75,22 +75,22 @@ assignments = {
       "title": "HW 11",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
       "dueDate": "2024-09-30T23:59:00",
-      "link": "https://www.gradescope.com/courses/831899",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/831899/assignments/4772482/submissions/273140535",
+      "submitted": true
     },
     {
       "title": "HW 12",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
       "dueDate": "2024-09-30T23:59:00",
-      "link": "https://www.gradescope.com/courses/831899",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/831899/assignments/4772488/submissions/273140623",
+      "submitted": true
     },
     {
       "title": "HW 13",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
       "dueDate": "2024-09-30T23:59:00",
-      "link": "https://www.gradescope.com/courses/831899",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/831899/assignments/4772490/submissions/273140679",
+      "submitted": true
     },
     {
       "title": "HW 14",
