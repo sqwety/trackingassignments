17be5d79ab9b28502a324f6d152299f0eaea3bbe
planit <planit@tanjeffreyz.github.io>
2025-01-01 02:10:09 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -5,266 +5,266 @@ assignments = {
     {
       "title": "HW 01",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-09-02T23:59:00",
+      "dueDate": "2025-09-02T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4771226/submissions/263914209",
       "submitted": true
     },
     {
       "title": "HW 02",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-09-02T23:59:00",
+      "dueDate": "2025-09-02T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4771287/submissions/263914238",
       "submitted": true
     },
     {
       "title": "HW 03",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-09-09T23:59:00",
+      "dueDate": "2025-09-09T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772470/submissions/265835746",
       "submitted": true
     },
     {
       "title": "HW 04",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-09-09T23:59:00",
+      "dueDate": "2025-09-09T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772472/submissions/265835796",
       "submitted": true
     },
     {
       "title": "HW 05",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-09-16T23:59:00",
+      "dueDate": "2025-09-16T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772473/submissions/268174866",
       "submitted": true
     },
     {
       "title": "HW 06",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-09-16T23:59:00",
+      "dueDate": "2025-09-16T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772474/submissions/268174926",
       "submitted": true
     },
     {
       "title": "HW 07",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-09-16T23:59:00",
+      "dueDate": "2025-09-16T23:59:00",
       "link": "https://www.gradescope.com/courses/831899",
       "submitted": false
     },
     {
       "title": "HW 08",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-09-23T23:59:00",
+      "dueDate": "2025-09-23T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772478/submissions/270746208",
       "submitted": true
     },
     {
       "title": "HW 09",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-09-23T23:59:00",
+      "dueDate": "2025-09-23T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772480/submissions/270746043",
       "submitted": true
     },
     {
       "title": "HW 10",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-09-23T23:59:00",
+      "dueDate": "2025-09-23T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772481/submissions/270746493",
       "submitted": true
     },
     {
       "title": "HW 11",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-09-30T23:59:00",
+      "dueDate": "2025-09-30T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772482/submissions/273140535",
       "submitted": true
     },
     {
       "title": "HW 12",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-09-30T23:59:00",
+      "dueDate": "2025-09-30T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772488/submissions/273140623",
       "submitted": true
     },
     {
       "title": "HW 13",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-09-30T23:59:00",
+      "dueDate": "2025-09-30T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772490/submissions/273140679",
       "submitted": true
     },
     {
       "title": "HW 14",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-10-07T23:59:00",
+      "dueDate": "2025-10-07T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772492/submissions/275617969",
       "submitted": true
     },
     {
       "title": "HW 15",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-10-07T23:59:00",
+      "dueDate": "2025-10-07T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772494/submissions/275618191",
       "submitted": true
     },
     {
       "title": "HW 16",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-10-14T23:59:00",
+      "dueDate": "2025-10-14T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772495/submissions/278013338",
       "submitted": true
     },
     {
       "title": "HW 17",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-10-14T23:59:00",
+      "dueDate": "2025-10-14T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772497/submissions/278013403",
       "submitted": true
     },
     {
       "title": "HW 18",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-10-14T23:59:00",
+      "dueDate": "2025-10-14T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772498/submissions/278013496",
       "submitted": true
     },
     {
       "title": "HW 19",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-10-21T23:59:00",
+      "dueDate": "2025-10-21T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772505/submissions/280602829",
       "submitted": true
     },
     {
       "title": "HW 20",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-10-21T23:59:00",
+      "dueDate": "2025-10-21T23:59:00",
       "link": "https://www.gradescope.com/courses/831899",
       "submitted": false
     },
     {
       "title": "HW 21",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-10-21T23:59:00",
+      "dueDate": "2025-10-21T23:59:00",
       "link": "https://www.gradescope.com/courses/831899",
       "submitted": false
     },
     {
       "title": "HW 22",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-10-28T23:59:00",
+      "dueDate": "2025-10-28T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772508/submissions/283153485",
       "submitted": true
     },
     {
       "title": "HW 23",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-10-28T23:59:00",
+      "dueDate": "2025-10-28T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772509/submissions/283153307",
       "submitted": true
     },
     {
       "title": "HW 24",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-10-28T23:59:00",
+      "dueDate": "2025-10-28T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772510/submissions/283184751",
       "submitted": true
     },
     {
       "title": "HW 25",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-11-04T23:59:00",
+      "dueDate": "2025-11-04T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772511/submissions/285604896",
       "submitted": true
     },
     {
       "title": "HW 26",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-11-04T23:59:00",
+      "dueDate": "2025-11-04T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772519/submissions/285605048",
       "submitted": true
     },
     {
       "title": "HW 27",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-11-04T23:59:00",
+      "dueDate": "2025-11-04T23:59:00",
       "link": "https://www.gradescope.com/courses/831899",
       "submitted": false
     },
     {
       "title": "HW 28",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-11-11T23:59:00",
+      "dueDate": "2025-11-11T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772521/submissions/288072844",
       "submitted": true
     },
     {
       "title": "HW 29",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-11-12T23:59:00",
+      "dueDate": "2025-11-12T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772522/submissions/288223158",
       "submitted": true
     },
     {
       "title": "HW 30",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-11-18T23:59:00",
+      "dueDate": "2025-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772523/submissions/290595106",
       "submitted": true
     },
     {
       "title": "HW 31",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-11-18T23:59:00",
+      "dueDate": "2025-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772524/submissions/290595363",
       "submitted": true
     },
     {
       "title": "HW 32",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-11-25T23:59:00",
+      "dueDate": "2025-11-25T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772525/submissions/292928900",
       "submitted": true
     },
     {
       "title": "HW 33",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-11-25T23:59:00",
+      "dueDate": "2025-11-25T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772526/submissions/292929021",
       "submitted": true
     },
     {
       "title": "HW 34",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-11-25T23:59:00",
+      "dueDate": "2025-11-25T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772527/submissions/292929685",
       "submitted": true
     },
     {
       "title": "HW 35",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-12-02T23:59:00",
+      "dueDate": "2025-12-02T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772529/submissions/294085860",
       "submitted": true
     },
     {
       "title": "HW 36",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-12-09T23:59:00",
+      "dueDate": "2025-12-09T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772531/submissions/296210694",
       "submitted": true
     },
     {
       "title": "HW 37",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-12-09T23:59:00",
+      "dueDate": "2025-12-09T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772535/submissions/296210864",
       "submitted": true
     },
     {
       "title": "HW 38",
       "course": "MATH 54 LEC 1-2 HWs and Quizzes",
-      "dueDate": "2024-12-09T23:59:00",
+      "dueDate": "2025-12-09T23:59:00",
       "link": "https://www.gradescope.com/courses/831899/assignments/4772536/submissions/296210788",
       "submitted": true
     }
