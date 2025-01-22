f6670a970fac7400ea84bbfe1655c4b15de37b66
planit <planit@tanjeffreyz.github.io>
2025-01-22 20:35:51 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,39 @@
 assignments = {
-  "Data 100": []
+  "Data 100": [
+    {
+      "title": "Syllabus Quiz",
+      "course": "Data 100",
+      "dueDate": "2025-01-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/949942/assignments/5620365/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Homework 1 Coding",
+      "course": "Data 100",
+      "dueDate": "2025-01-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/949942",
+      "submitted": false
+    },
+    {
+      "title": "Homework 1 Coding Written",
+      "course": "Data 100",
+      "dueDate": "2025-01-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/949942",
+      "submitted": false
+    },
+    {
+      "title": "Homework 1 (Math Prereqs)",
+      "course": "Data 100",
+      "dueDate": "2025-01-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/949942",
+      "submitted": false
+    },
+    {
+      "title": "Lab 01",
+      "course": "Data 100",
+      "dueDate": "2025-01-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/949942",
+      "submitted": false
+    }
+  ]
 };
