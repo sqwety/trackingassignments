5060e418c08c28a5e278d7e426f7fd55a6b66715
planit <planit@tanjeffreyz.github.io>
2025-01-28 01:54:53 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -4,8 +4,8 @@ assignments = {
       "title": "Syllabus Quiz",
       "course": "Data 100",
       "dueDate": "2025-01-30T23:59:00",
-      "link": "https://www.gradescope.com/courses/949942/assignments/5620365/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/949942/assignments/5620365/submissions/302566221",
+      "submitted": true
     },
     {
       "title": "Homework 1 Coding",
