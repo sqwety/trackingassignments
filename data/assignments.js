33a3405a8122bd610b930b059a1891c58a980b19
planit <planit@tanjeffreyz.github.io>
2025-03-12 03:45:02 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -188,8 +188,8 @@ assignments = {
       "title": "Lab 06",
       "course": "Data 100",
       "dueDate": "2025-03-11T23:59:00",
-      "link": "https://www.gradescope.com/courses/949942",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/949942/assignments/5877909/submissions/316443929",
+      "submitted": true
     }
   ],
   "PHYSICS 7B-LEC-001-Spring 2025": [
