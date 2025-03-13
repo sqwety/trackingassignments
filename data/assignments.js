69a08d0dc6b773bf04aa363c80c4ff57e09eb1ed
planit <planit@tanjeffreyz.github.io>
2025-03-13 02:09:16 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -190,6 +190,13 @@ assignments = {
       "dueDate": "2025-03-11T23:59:00",
       "link": "https://www.gradescope.com/courses/949942/assignments/5877909/submissions/316443929",
       "submitted": true
+    },
+    {
+      "title": "Midterm (Online)",
+      "course": "Data 100",
+      "dueDate": "2025-03-12T22:10:00",
+      "link": "https://www.gradescope.com/courses/949942",
+      "submitted": false
     }
   ],
   "PHYSICS 7B-LEC-001-Spring 2025": [
