5d56521b475a8f71ce40e3388bb066e114e08d50
planit <planit@tanjeffreyz.github.io>
2025-04-03 02:11:20 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -251,8 +251,8 @@ assignments = {
       "title": "Project A2 Test Set Predictions",
       "course": "Data 100",
       "dueDate": "2025-04-03T23:59:00",
-      "link": "https://www.gradescope.com/courses/949942",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/949942/assignments/5951136/submissions/321995617",
+      "submitted": true
     },
     {
       "title": "Project A2 Written",
